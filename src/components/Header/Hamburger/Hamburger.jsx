943598f93a65b981ce8998e553ccaf4487412894
PlumpAndProud <f.machi03@gmail.com>
2023-11-15 20:13:09 +0100

    animation for li

--- src/components/Header/Hamburger/Hamburger.jsx
+++ src/components/Header/Hamburger/Hamburger.jsx
@@ -2,6 +2,7 @@ import { Squash as Hamburger } from "hamburger-react";
 import { useState } from "react";
 import { useRef } from "react";
 import { useClickAway } from "react-use";
+import { AnimatePresence, motion, spring } from "framer-motion";
 import style from "./Hamburger.module.scss";
 
 export default function HamburgerSection() {
@@ -21,14 +22,44 @@ export default function HamburgerSection() {
           label="Show menu"
           rounded
         />
-        {isOpen && (
-          <div className={style.Project_list}>
-            <ul>
-              <li>SIema</li>
-              <li>Siema</li>
-            </ul>
-          </div>
-        )}
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2 }}
+              className={style.Project_list}
+            >
+              <ul>
+                <motion.li
+                  initial={{ scale: 0, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 150,
+                    damping: 20,
+                    delay: 0.2,
+                  }}
+                >
+                  SIema
+                </motion.li>
+                <motion.li
+                  initial={{ scale: 0, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 150,
+                    damping: 20,
+                    delay: 0.4,
+                  }}
+                >
+                  Siema
+                </motion.li>
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
         <div className={style.Project_navigation}>Project Navigation</div>
       </div>
     </>
